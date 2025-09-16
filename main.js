e0dc37144db4a91f1d63e542bca4bce6e88e3298
ccd2s <tasaedem@outlook.com>
2025-09-17 01:41:11 +0800

    feat: 添加 I18n 插件

--- main.js
+++ main.js
@@ -1,75 +1,12 @@
-
-//鹰：可以包含其他文件：
-//.import 'XXX.js' as JsName
-//console.debug(JsName.aaa);
-
-
-
 //游戏开始脚本（开始时调用）
-function *$start() {
+function* $start() {
+    I18n.init();
+    if (!game.cd['i18nStatus']) console.error("[I18n]","语言加载失败");
+    yield game.msg(I18n.c("helloWorld"));
+    yield game.msg(I18n.c("v",Math.floor(Math.random() * (20 - 10)),Math.floor(Math.random() * (20 - 10))));
+    yield game.msg(I18n.f("main","title",game.gf['i18nVersion']));
+    yield game.msg(I18n.info(0));
+    const status = I18n.changeLanguage("zh");
+    if (status) yield game.msg(game.cd['i18nLanguage']);
 
-
-    while(1) {
-        let c = yield game.menu("请选择", ["开始游戏","读取存档","游戏说明","制作人员"]);
-        switch(c) {
-            case 0:
-                
-game.scale(1);
-game.interval(16);
-//game.loadmap('鹰歌地图');
-//game.createhero('深林孤鹰');
-//game.movehero(1,11);
-//game.playmusic('音乐1mp3');
-yield game.msg('Hello World<br>欢迎来到鹰歌Maker，这是一个最简单的demo，如果需要体验完整游戏请点击 示例工程 或加群下载更多工程！');
-game.goon();
-
-
-                break;
-            case 1:
-                /*let arrSave = [];
-                for(let i = 0; i < 3; ++i) {
-                    let ts = game.checksave('存档' + i);
-                    if(ts) {
-                        arrSave.push('存档%1：%2（%3）'.arg(i).arg(ts.Name).arg(ts.Time));
-                    }
-                    else
-                        arrSave.push('空');
-                }*/
-
-                let readSavesInfo = game.$userscripts.$readSavesInfo || game.$gameMakerGlobalJS.$readSavesInfo;
-                let arrSave = readSavesInfo();
-
-                c = yield game.menu("载入存档", [...arrSave,"自动存档","取消"]);
-                switch(c) {
-                    case 0:
-                    case 1:
-                    case 2:
-                        //game.$globalLibraryJS.runNextEventLoop(function() {game.load('存档' + c)},);
-                        if(game.load('存档' + c))
-                            break;
-                        else
-                            yield game.msg("读取失败");
-                        continue;
-                    case 3:
-                        //game.$globalLibraryJS.runNextEventLoop(function() {game.load('autosave')},);
-                        if(game.load('autosave'))
-                            break;
-                        else
-                            yield game.msg("读取失败");
-                        continue;
-                    default:
-                        continue;
-                }
-                break;
-            case 2:
-                yield game.msg("这里写游戏说明");
-                continue;
-            case 3:
-                yield game.msg("框架和引擎开发：深林孤鹰", 60, '', 0);
-                continue;
-        }
-
-        break;
-    }
 }
-        
